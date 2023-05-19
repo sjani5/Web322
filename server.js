57f4ee69312fbf83a9122313c9df27ce78062a51
Shubh Jani <sjani5@myseneca.ca>
2023-05-19 15:38:57 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
+/**
+* WEB322 - Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic
+Policy.
+* No part of this assignment has been copied manually or electronically from any other
+source
+* (including web sites and friends) or distributed to other students.
+* I understand that if caught doing so, I will receive zero on this assignment and
+possible
+* fail the entire course.
+*
+* Name:SHUBH JIGNESHBHAI JANI
+* Student ID: 153660212
+* Date: 19th May 2023
+* Cyclic Web App URL: https://difficult-garters-clam.cyclic.app/
+* GitHub Repository URL:https://github.com/sjani5/Web322/
+*
+*/
+
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
